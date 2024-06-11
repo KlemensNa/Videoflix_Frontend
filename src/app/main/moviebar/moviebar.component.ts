cd14784404e43ff3cc62bf64263206138da55891
KlemensNa <klemens.naue@gmail.com>
2024-06-11 21:35:57 +0200

    add categories and render videos in categories

--- src/app/main/moviebar/moviebar.component.ts
+++ src/app/main/moviebar/moviebar.component.ts
@@ -11,6 +11,12 @@ import { VideoService } from 'src/app/services/video.service';
 export class MoviebarComponent {
 
   videos: any[] = [];
+  ballsport: any[] = [];
+  ussport:any[] = [];
+  golf: any[] = [];
+  football: any[] = [];
+  handball: any[] = [];
+  basketball: any[] = [];
   videoOpened: boolean = false;
 
   constructor(private videoService: VideoService) { }
@@ -18,7 +24,10 @@ export class MoviebarComponent {
   ngOnInit(): void {
     this.videoService.getVideos().subscribe(data => {
       this.videos = data;
+      this.sortVideosToCategory()
     });
+    console.warn("Golfen", this.golf)
+    console.warn("Fußball", this.football)
   }
 
   getVideoUrl(videoPath: string): string {
@@ -26,14 +35,61 @@ export class MoviebarComponent {
   }
 
   toggleVideo(video: any){
-
     console.log(video)
-      video.isplaying =! video.isplaying
+      video.isplaying =! video.isplaying      
+  }
 
-      
+  sortVideosToCategory(){
+
+    this.videos.forEach(video => {      
+      this.sortToSpecificCategory(video)
+      if(video.category){
+        console.error(video.category)
+        this.sortToMainCategory(video)
+      }
+    });
     
   }
 
+  sortToMainCategory(video: any){
+    switch (video.category.toLowerCase()) {
+      case 'ballsport':
+        this.ballsport.push(video);
+        break;
+      case 'ussport':
+        this.ussport.push(video);
+        break;
+      // case 'handball':
+      //   this.handball.push(video);
+      //   break;
+      // case 'basketball':
+      //   this.basketball.push(video);
+      //   break;
+      default:
+        // Handle case where category doesn't match any predefined categories
+        console.warn(`Unknown category: ${video.category}`);
+    }    
+  }
+
+  sortToSpecificCategory(video: any){
+    switch (video.sport.toLowerCase()) {
+      case 'golf':
+        this.golf.push(video);
+        break;
+      case 'football':
+        this.football.push(video);
+        break;
+      case 'handball':
+        this.handball.push(video);
+        break;
+      case 'basketball':
+        this.basketball.push(video);
+        break;
+      default:
+        // Handle case where category doesn't match any predefined categories
+        console.warn(`Unknown category: ${video.category}`);
+    }    
+  }
 
 
 
